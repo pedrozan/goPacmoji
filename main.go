4a140f0ea0433fed4778c20bae3d224346245d95
Pedro Henrique <pedro.schleder@cwi.com.br>
2019-10-19 12:28:58 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// initialize game
+
+	// load resources
+
+	// game loop
+	for {
+		// update screen
+
+		// process input
+
+		// process movement
+
+		// process colisions
+
+		// check game over
+
+		// Temp: break infinite loop
+		fmt.Println("Hello, Pac Go!")
+		break
+
+		// repeat
+	}
+}
