159972d9a8b15e842802836aa5f4783885d1e6df
Pedro Henrique <pedro.schleder@cwi.com.br>
2019-10-19 12:40:39 -0300

    printing map

--- main.go
+++ main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+var maze []string
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
 
 func main() {
 	// initialize game
 
 	// load resources
+	err := loadMaze()
+	if err != nil {
+		log.Printf("Error loading maze: %v\n", err)
+		return
+	}
 
 	// game loop
 	for {
 		// update screen
+		printScreen()
 
 		// process input
 
@@ -24,7 +55,6 @@ func main() {
 		// check game over
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
 		break
 
 		// repeat
